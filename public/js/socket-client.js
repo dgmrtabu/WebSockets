80831a1f69042ca0ec9925654cccc582b164c310
DAVID GABRIEL MONTAÑO ROCHA <david.montano@cns.gob.bo>
2021-08-16 16:21:27 -0400

    C-205 Emitir desde el servidor

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,19 @@
+const lblOnline = document.querySelector("#lblOnLine");
+const lblOffline = document.querySelector("#lblOffLine");
+
+const txtMensaje = document.querySelector("#txtMensaje");
+const btnEnviar = document.querySelector("#btnEnviar");
+
 const socket = io();
+
+socket.on("connect", () => {
+  console.log("Conectado");
+  lblOffline.style.display = "none";
+  lblOnline.style.display = "";
+});
+
+socket.on("disconnect", () => {
+  console.log("Desconectado del servidor");
+  lblOffline.style.display = "";
+  lblOnline.style.display = "none";
+});
